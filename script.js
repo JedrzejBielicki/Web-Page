dd54a9084f8f172a620a5709818993f1b8b5a30d
Jędrzej <jedrzejb123@gmail.com>
2020-06-16 10:01:42 +0200

    menu styling

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const navBtn = document.querySelector('.nav__btn');
+const menuCircle = document.querySelector('.circle');
 
 
 navBtn.addEventListener('click', () => {
-
+    menuCircle.classList.toggle('circle--active');
     navBtn.classList.toggle('open');
 })
