a9aba2cfa7d1a9202f16d3ae74f18faf50023694
Jędrzej <jedrzejb123@gmail.com>
2020-06-16 13:22:52 +0200

    add animations

--- script.js
+++ script.js
@@ -11,9 +11,26 @@ function parallax(element, distance, speed) {
     const item = document.querySelector(element);
     item.style.transform = `translateX(${distance * speed}px)`;
 }
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", () => {
     parallax(".main__line1", window.scrollY, 1.5);
     parallax(".main__line2", window.scrollY, 1.3);
     parallax(".main__line3", window.scrollY, 1.1);
+})
 
+// window.addEventListener('resize', () => {
+//     let mainHeight = document.querySelector('.main').clientHeight;
+//     console.log(mainHeight)
+// })
+let mainHeight = document.querySelector('.main').clientHeight;
+const stuff = document.querySelector('.stuff');
+const lineContainer = document.querySelector('.line__container')
+document.addEventListener('scroll', () => {
+
+    if (scrollY > mainHeight / 2) {
+        stuff.classList.remove('stuff--up')
+        lineContainer.classList.remove('line__container--up')
+    } else {
+        stuff.classList.add('stuff--up')
+        lineContainer.classList.add('line__container--up')
+    }
 })
