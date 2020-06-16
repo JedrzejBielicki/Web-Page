9d6c1bd65209b8fda49858662bcb5ec6fe9591a3
Jędrzej <jedrzejb123@gmail.com>
2020-06-16 12:19:50 +0200

    styling, add stuff section

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ navBtn.addEventListener('click', () => {
     menuCircle.classList.toggle('circle--active');
     navBtn.classList.toggle('open');
 })
+
+function parallax(element, distance, speed) {
+    const item = document.querySelector(element);
+    item.style.transform = `translateX(${distance * speed}px)`;
+}
+window.addEventListener("scroll", function () {
+    parallax(".main__line1", window.scrollY, 1.5);
+    parallax(".main__line2", window.scrollY, 1.3);
+    parallax(".main__line3", window.scrollY, 1.1);
+
+})
