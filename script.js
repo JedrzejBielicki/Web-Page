99e3195b8ebb3437eb2b4d064eee305f704db5ed
Jędrzej <jedrzejb123@gmail.com>
2020-06-22 16:40:46 +0200

    stuff section styling

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const stuffHeader = document.querySelector('.stuff__header');
 const aboutSec = document.querySelector('.about');
 const aboutLineContainer = document.querySelector('.about__line__container');
 const navigationNumbers = document.querySelectorAll('.navigation__item');
+const stuffContainer = document.querySelector('.stuff__container');
 
 
 document.addEventListener('scroll', () => {
@@ -15,20 +16,22 @@ document.addEventListener('scroll', () => {
     if (scrollY > mainHeight / 2 && scrollY < mainHeight * 1.5) {
         stuff.classList.remove('stuff--up')
         stuffLineContainer.classList.remove('stuff__line__container--up')
-
+        stuffContainer.classList.remove('stuff__container--up');
     } else {
         stuff.classList.add('stuff--up')
         stuffLineContainer.classList.add('stuff__line__container--up')
-
+        stuffContainer.classList.add('stuff__container--up');
     }
 })
 document.addEventListener('scroll', () => {
     if (scrollY > mainHeight + (stuffHeight / 2)) {
         aboutSec.classList.remove('about--up');
         aboutLineContainer.classList.remove('about__line__container--up');
+
     } else {
         aboutSec.classList.add('about--up');
         aboutLineContainer.classList.add('about__line__container--up');
+
     }
 })
 document.addEventListener('scroll', () => {
